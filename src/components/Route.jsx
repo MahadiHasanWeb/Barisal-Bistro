ba9499e7c221d4cc9740fb72fa44826e18cd88a0
MahadiHasanWeb <muhammad.mahadi.hasan.9@gmail.com>
2023-05-02 20:36:05 +0600

    Error page added

--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -3,11 +3,13 @@ import Navbar from "./Navbar/Navbar";
 import Home from "./Home/Home";
 import Layout from "./Layout/Layout";
 import Blog from "./Blog/Blog";
+import ErrorPage from "./ErrorPage/ErrorPage";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout></Layout>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
